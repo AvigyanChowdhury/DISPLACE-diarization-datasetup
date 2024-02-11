7dc64ce1fa57bf1ed8fb9cd9ff2465a4d3eb40eb
AvigyanChowdhury <146307887+AvigyanChowdhury@users.noreply.github.com>
2024-02-11 10:24:25 +0530

    add changes to database.yml
    add links to download displace wav files in .sh file

---
+++ pyannote/download_displace.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+      DLFOLDER="displace"
+else
+      DLFOLDER="$1/displace"
+fi
+
+
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B007/audio https://drive.google.com/uc?export=download&id=1aN6cWArpJB4pD22QyQk1m7fpjXKMceO1
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B010/audio https://drive.google.com/uc?export=download&id=1H4Rzk19UtXTpgz7UbpkmM3RDUH-V4Edc
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B014/audio https://drive.google.com/uc?export=download&id=1HNfzpInCq_MB271kV0qudLRIqQZarffh
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B021/audio https://drive.google.com/uc?export=download&id=1-kAHODWKUcWAf840RX8kh6UYalXzROnU
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B022/audio https://drive.google.com/uc?export=download&id=1jk0UgMktpk5wIvMLywnHRWTtxGr7qzEd
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B024/audio https://drive.google.com/uc?export=download&id=1KHJFwx_Re5BbHSlDNGTkwsun_sCE2xG-
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B025/audio https://drive.google.com/uc?export=download&id=1zOxOnrm-gVlPJR-GLlB8vu6vZMDeZx1z
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B026/audio https://drive.google.com/uc?export=download&id=19f8asW0GZSz5NUC_YVxugvQ4IeeQ3I2n
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B034/audio https://drive.google.com/uc?export=download&id=1XiWV1yaZ0wRNAoA3e6sAYp73Hy_FKCNV
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B036/audio https://drive.google.com/uc?export=download&id=1RglHtzDGQ1tHBkzOS1GSCx788oRczZNB
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B038/audio https://drive.google.com/uc?export=download&id=1IeQKMRciNdr1DPBEKRuXafGvdMV-OTm6
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B040/audio https://drive.google.com/uc?export=download&id=1T8eUNKC2sT0VB9OtC3hrWKa_aZNUDHCZ
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B042/audio https://drive.google.com/uc?export=download&id=1QawshL_AxsVuN_lM8A5NF8wNBWZYtKzm
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B044/audio https://drive.google.com/uc?export=download&id=1y8KaFPg6T-B-xuxAfNMye6v2jMBXOdes
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B046/audio https://drive.google.com/uc?export=download&id=17Hx0GJRHMS_TN0Di5N6oOfo9QqORNfhF
+wget --no-verbose --show-progress --continue -P $DLFOLDER/B047/audio https://drive.google.com/uc?export=download&id=1Ftim5dccQjlC51OG5qRSHYrzyc68Ilww
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M016/audio https://drive.google.com/uc?export=download&id=15jRAlEmpPFzFp--ovPwdrBLzhlHPSCRC
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M018/audio https://drive.google.com/uc?export=download&id=1B08_1XVX7sHwZRuqTJdMK_KMVSDS8Pho
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M019/audio https://drive.google.com/uc?export=download&id=12JWTzrbOHsklmzlZH83jhjWdS-b2GmLP
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M020/audio https://drive.google.com/uc?export=download&id=10q3Tp2p2OGcloesakL0ucKKM73mJ4TuP
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M022/audio https://drive.google.com/uc?export=download&id=1lrkQi8h1qrHbv99NQfGpxNtFXTgIteX2
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M023/audio https://drive.google.com/uc?export=download&id=1Vqrop2D4-kvCYIc56LDEuCb__I966TbP
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M026/audio https://drive.google.com/uc?export=download&id=1cgKEBdMwfbeTkP9IW8Blaba5kSW1L0Xq
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M027/audio https://drive.google.com/uc?export=download&id=1A6LhH8FBqlvOnNugbDM3Sikb_fnA_Kja
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M028/audio https://drive.google.com/uc?export=download&id=1Q8N0z19x49X90i4JakkyDROC0RSam7Yd
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M029/audio https://drive.google.com/uc?export=download&id=1x1zz5MlgOg4U9RSwO5TynuMMX3QSRf9l
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M030/audio https://drive.google.com/uc?export=download&id=1Oh4OIv7S6CobcHp8YXJTywithK2Bxd_t
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M031/audio https://drive.google.com/uc?export=download&id=1rct7cDa700aS3MIeN176bYUmnLD_W9VD
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M032/audio https://drive.google.com/uc?export=download&id=1p8r3gxyG1bdoIxxt1RjdRErvYxz9h7pR
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M033/audio https://drive.google.com/uc?export=download&id=1nxxyquck4cidRwEtuWKHtV1sfhqhtNI_
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M037/audio https://drive.google.com/uc?export=download&id=1iOsC1HtewDZXAZAwEJk9HROO_2s_WAk6
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M038/audio https://drive.google.com/uc?export=download&id=1HqPQdHuB0fHtDsda0ewn5cu85XjeXy0C
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M039/audio https://drive.google.com/uc?export=download&id=1azYzeiG6GNXP5l0lLHy3WCGkq8aFPF4g
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M040/audio https://drive.google.com/uc?export=download&id=1i4Vv48tpbLzZC8UKif0IZ6Fnk4YBYPEa
+wget --no-verbose --show-progress --continue -P $DLFOLDER/M043/audio https://drive.google.com/uc?export=download&id=1CXK9IuBudLhDyvP7kD7r0EzBL25wmLZv
+
